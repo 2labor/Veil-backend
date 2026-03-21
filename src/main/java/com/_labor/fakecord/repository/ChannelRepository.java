a108d6b1a04cba9b0e504148d0737c7c5fd05e08
2labor <yaroslavkliutko@gmail.com>
2026-03-21 00:23:14 +0000

    add lastActivityAt to Channel and implemet methods for usage in repository

--- src/main/java/com/_labor/fakecord/repository/ChannelRepository.java
+++ src/main/java/com/_labor/fakecord/repository/ChannelRepository.java
@@ -1,6 +1,8 @@
 package com._labor.fakecord.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -14,4 +16,13 @@ public interface ChannelRepository extends JpaRepository<Channel, Long>{
   @Modifying
   @Query("UPDATE Channel c SET c.position = :position WHERE c.id = :id")
   void updatePosition(Long id, Integer position);
+
+  @Modifying
+  @Query("UPDATE Channel c SET c.lastActivityAt = :lastActivity WHERE c.id = :id")
+  void updateLastActivity(Long id, LocalDateTime lastActivity);
+
+  @Query("SELECT c FROM Channel c JOIN ChannelMember cm ON c.id = cm.id.channelId " +
+    "WHERE cm.id.userId = :userId AND c.type = 'DM' " +
+    "ORDER BY c.lastActivityAt DESC")
+  List<Channel> findActiveDirectMessages(UUID userId);
 }
