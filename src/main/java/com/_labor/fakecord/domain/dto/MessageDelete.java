5e6caca2b20bef58db184e2d53f87be548b375fc
2labor <yaroslavkliutko@gmail.com>
2026-04-01 19:00:31 +0100

    refactor: implement NotificationPayload in DTO records

--- src/main/java/com/_labor/fakecord/domain/dto/MessageDelete.java
+++ src/main/java/com/_labor/fakecord/domain/dto/MessageDelete.java
@@ -1,9 +1,10 @@
 package com._labor.fakecord.domain.dto;
 
+import com._labor.fakecord.domain.notifications.NotificationPayload;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
 public record MessageDelete(
   @JsonSerialize(using = ToStringSerializer.class)
   Long messageId  
-) {}
+) implements NotificationPayload {}
--- src/main/java/com/_labor/fakecord/domain/dto/MessageDto.java
+++ src/main/java/com/_labor/fakecord/domain/dto/MessageDto.java
@@ -1,6 +1,7 @@
 package com._labor.fakecord.domain.dto;
 
 import com._labor.fakecord.domain.enums.MessageType;
+import com._labor.fakecord.domain.notifications.NotificationPayload;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 public record MessageDto (
@@ -12,4 +13,4 @@ public record MessageDto (
   String nonce,
   Long updatesAt,
   Long timestamp
-) {}
+) implements NotificationPayload {}
