a912fa5c9d09a2097f5a6d4a8fbb200a749b676f
2labor <yaroslavkliutko@gmail.com>
2026-07-28 04:00:05 +0100

    feat(controller): add GET /api/v1/servers/{serverId}/members/sidebar endpoint

---
+++ src/main/java/com/_labor/fakecord/controller/ServerMemberController.java
@@ -0,0 +1,53 @@
+package com._labor.fakecord.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com._labor.fakecord.domain.dto.ServerMemberSidebarResponseDto;
+import com._labor.fakecord.services.ServerMemberService;
+
+import lombok.RequiredArgsConstructor;
+
+import java.security.Principal;
+import java.util.UUID;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/api/v1/servers/{serverId}/members")
+@RequiredArgsConstructor
+public class ServerMemberController {
+  private final ServerMemberService service;
+
+  @GetMapping("/sidebar")
+  public ResponseEntity<Slice<ServerMemberSidebarResponseDto>> getSideBarUsers(
+    Principal principal,
+    @PathVariable Long serverId,
+    @RequestParam(defaultValue = "0") int page,
+    @RequestParam (defaultValue = "30") int limit
+  ) {
+    UUID userId = getUserId(principal);
+
+    int size = Math.min(limit, 40);
+
+    Pageable pageable = PageRequest.of(page, size, Sort.by("userLocalName").ascending());
+
+    Slice<ServerMemberSidebarResponseDto> sidebarSlice = service.getServerMembers(serverId, userId, pageable);
+
+    return ResponseEntity.ok(sidebarSlice);
+  }
+
+  private UUID getUserId(Principal principal) {
+    if (principal == null) return null;
+
+    return UUID.fromString(principal.getName());
+  }
+}
