fb2f4fec21d59360e08c4e5251b4faa315dc9c6e
2labor <yaroslavkliutko@gmail.com>
2026-07-28 03:45:02 +0100

    feat(server): implement batch member sidebar fetching logic in ServerMembersService

--- src/main/java/com/_labor/fakecord/repository/ServerMemberRepository.java
+++ src/main/java/com/_labor/fakecord/repository/ServerMemberRepository.java
@@ -2,15 +2,25 @@ package com._labor.fakecord.repository;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com._labor.fakecord.domain.entity.ServerMember;
 import com._labor.fakecord.domain.entity.ServerMemberId;
 
+import io.lettuce.core.dynamic.annotation.Param;
+
 @Repository
 public interface ServerMemberRepository extends JpaRepository<ServerMember, ServerMemberId> {
   boolean existsById_ServerIdAndId_UserId(Long serverId, UUID userId);
+  @Query("SELECT DISTINCT sm FROM ServerMember sm " +
+    "LEFT JOIN FETCH sm.roles r " +
+    "WHERE sm.id.serverId = :serverId " +
+    "ORDER BY sm.userLocalName ASC")
+  Slice<ServerMember> findAllByServerIdWithRoles(@Param("serverId") Long serverId, Pageable pageable);
   // ServerMember findByUserId(UUID userId);
   // ServerMember findByServerId(Long serverId);
 }
