753a50c9187983384e8201c175c81a02e93190f6
2labor <yaroslavkliutko@gmail.com>
2026-07-28 03:46:06 +0100

    feat(domain): add sidebar DTOs, topRole extraction logic, and mappers

---
+++ src/main/java/com/_labor/fakecord/domain/mappper/Impl/TopRoleMapperImpl.java
@@ -0,0 +1,24 @@
+package com._labor.fakecord.domain.mappper.Impl;
+
+import org.springframework.stereotype.Component;
+
+import com._labor.fakecord.domain.dto.TopRoleDto;
+import com._labor.fakecord.domain.entity.ServerRole;
+import com._labor.fakecord.domain.mappper.TopRoleMapper;
+
+@Component
+public class TopRoleMapperImpl implements TopRoleMapper {
+
+  @Override
+  public TopRoleDto toDto(ServerRole entity) {
+    if (entity == null) return null;
+
+    return TopRoleDto.builder()
+      .id(entity.getId())
+      .position(entity.getPosition())
+      .name(entity.getName())
+      .hexColor(entity.getColorHex())
+      .build();
+  }
+  
+}
--- src/main/java/com/_labor/fakecord/services/impl/ServerMemberServiceImpl.java
+++ src/main/java/com/_labor/fakecord/services/impl/ServerMemberServiceImpl.java
@@ -1,11 +1,22 @@
 package com._labor.fakecord.services.impl;
 
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
+import com._labor.fakecord.domain.dto.ServerMemberSidebarResponseDto;
+import com._labor.fakecord.domain.dto.UserProfileShort;
+import com._labor.fakecord.domain.entity.ServerMember;
+import com._labor.fakecord.domain.mappper.ServerMemberMapper;
 import com._labor.fakecord.repository.ServerMemberRepository;
 import com._labor.fakecord.services.ServerMemberService;
+import com._labor.fakecord.services.UserProfileCache;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,10 +25,32 @@ import lombok.RequiredArgsConstructor;
 public class ServerMemberServiceImpl implements ServerMemberService {
 
   private final ServerMemberRepository repository;
+  private final ServerMemberMapper mapper;
+  private final UserProfileCache userProfileService;
 
   @Override
   public boolean checkIsUserMember(Long serverId, UUID userId) {
     return repository.existsById_ServerIdAndId_UserId(serverId, userId);
+  }
+
+  @Override
+  public Slice<ServerMemberSidebarResponseDto> getServerMembers(Long serverId, UUID currentId, Pageable pageable) {
+    if (!checkIsUserMember(serverId, currentId)){
+      throw new AccessDeniedException("You do not have access to this server!");
+    }
+
+    Slice<ServerMember> members = repository.findAllByServerIdWithRoles(serverId, pageable);
+
+    List<UUID> userIds = members.stream().map(m -> m.getId().getUserId()).toList();
+
+    Map<UUID, UserProfileShort> userProfiles = userProfileService.getUserProfileShortBatch(userIds);
+
+    List<ServerMemberSidebarResponseDto> dtos = mapper.toSidebarDtos(
+      members.getContent(), 
+      userProfiles
+    );
+
+    return new SliceImpl<ServerMemberSidebarResponseDto>(dtos, pageable, members.hasNext());
   }
   
 }
