0f521e364b17bf48463ea4e5dae0465a5373e752
2labor <yaroslavkliutko@gmail.com>
2026-08-04 01:58:54 +0100

    feat(roles): add role DTOs, Java Records and mapper implementation

---
+++ src/main/java/com/_labor/fakecord/domain/mappper/Impl/ServerRoleMapperImpl.java
@@ -0,0 +1,62 @@
+package com._labor.fakecord.domain.mappper.Impl;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Component;
+
+import com._labor.fakecord.domain.dto.ServerRoleCreateDto;
+import com._labor.fakecord.domain.dto.ServerRoleResponseDto;
+import com._labor.fakecord.domain.dto.ServerRoleUpdateDto;
+import com._labor.fakecord.domain.entity.ServerRole;
+import com._labor.fakecord.domain.mappper.ServerRoleMapper;
+
+@Component
+public class ServerRoleMapperImpl implements ServerRoleMapper {
+
+  @Override
+  public ServerRole toEntity(ServerRoleCreateDto dto) {
+    if (dto == null) return null;
+    
+    return ServerRole.builder()
+      .name(dto.name())
+      .hoist(dto.hoist())
+      .colorHex(dto.colorHex())
+      .permissions(dto.permissions())
+      .build();
+  }
+
+  @Override
+  public ServerRole toEntity(ServerRoleUpdateDto dto, long roleId) {
+    if (dto == null) return null;
+
+    return ServerRole.builder()
+      .id(roleId)
+      .name(dto.name())
+      .hoist(dto.hoist())
+      .colorHex(dto.colorHex())
+      .permissions(dto.permissions())
+      .build();
+  }
+
+  @Override
+  public ServerRoleResponseDto toDto(ServerRole entity) {
+    if (entity == null) return null; 
+
+    return ServerRoleResponseDto.builder()
+      .id(entity.getId())
+      .name(entity.getName())
+      .hoist(entity.isHoist())
+      .colorHex(entity.getColorHex())
+      .permissions(entity.getPermissions())
+      .build();
+  }
+
+  @Override
+  public List<ServerRoleResponseDto> toDtoList(List<ServerRole> entities) {
+    if (entities == null || entities.isEmpty()) return List.of();
+
+    return entities.stream().map(this::toDto).collect(Collectors.toList());
+  }
+  
+}
---
+++ src/main/java/com/_labor/fakecord/domain/mappper/ServerRoleMapper.java
@@ -0,0 +1,15 @@
+package com._labor.fakecord.domain.mappper;
+
+import java.util.List;
+
+import com._labor.fakecord.domain.dto.ServerRoleCreateDto;
+import com._labor.fakecord.domain.dto.ServerRoleResponseDto;
+import com._labor.fakecord.domain.dto.ServerRoleUpdateDto;
+import com._labor.fakecord.domain.entity.ServerRole;
+
+public interface ServerRoleMapper {
+  ServerRole toEntity(ServerRoleCreateDto dto);
+  ServerRole toEntity(ServerRoleUpdateDto dto, long roleId);
+  ServerRoleResponseDto toDto(ServerRole entity);
+  List<ServerRoleResponseDto> toDtoList(List<ServerRole> entities);
+}
